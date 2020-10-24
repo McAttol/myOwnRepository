f79749cd40331338749054af851ba369c7755191
Ramon Saura <ramon.saura@gmail.com>
2020-10-24 18:22:25 +0200

    delete files that we don't use by the moment

--- ramon-saura/angular-practice/ng-fundamentals/src/app/app.module.ts
+++ ramon-saura/angular-practice/ng-fundamentals/src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { EventsAppComponent } from './event.app.component';
 
 @NgModule({
   declarations: [
